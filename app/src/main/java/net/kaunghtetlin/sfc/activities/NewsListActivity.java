bf787f602f55eb27cb5a3fbad026c369232246fe
KaungHtetLin <kaunghtetlin74.khl@gmail.com>
2017-11-24 16:28:39 +0630

    Detail screen with action button Drawer finish

--- app/src/main/java/net/kaunghtetlin/sfc/activities/NewsListActivity.java
+++ app/src/main/java/net/kaunghtetlin/sfc/activities/NewsListActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -16,7 +18,13 @@ import net.kaunghtetlin.sfc.R;
 import net.kaunghtetlin.sfc.adapters.NewsAdapter;
 import net.kaunghtetlin.sfc.delegates.NewsItemDelegate;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class NewsListActivity extends AppCompatActivity implements NewsItemDelegate{
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +33,16 @@ public class NewsListActivity extends AppCompatActivity implements NewsItemDeleg
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ButterKnife.bind(this,this);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+              /*  Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+
+              drawerLayout.openDrawer(GravityCompat.START);
             }
         });
 
